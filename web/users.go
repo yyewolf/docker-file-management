1c346a3afe7353c862d4bea2dae1c0848e7a58fa
Yewolf <yyewolf@gmail.com>
2023-11-23 17:37:56 +0100

    fix: use email

--- web/users.go
+++ web/users.go
@@ -30,7 +30,7 @@ func userPanel(c echo.Context) error {
 
 func userFile(c echo.Context) error {
 	// Check X-Forwarded-User header
-	username := c.Request().Header.Get("X-Forwarded-User")
+	username := c.Request().Header.Get("X-Forwarded-Email")
 	if username == "" {
 		return c.JSON(403, map[string]interface{}{
 			"message": "forbidden",
